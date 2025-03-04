e96cecc5390ddef3db1f412a2c1d2a6d5024f551
Aadarshdubey637 <dubeyaadarsh637@gmail.com>
2025-03-05 01:51:54 +0530

    Update basis.py

--- basis.py
+++ basis.py
@@ -4,3 +4,16 @@ num1=int(input("Enter a first number:"))
 num2=int(input("Enter a second number"))
 sum=num1+num2
 print("the sum is:",sum)
+
+(2) #program to find largest of three number
+a = int(input("Enter a first number: "))
+b = int(input("Enter a second number: "))
+c = int(input("Enter a third number: "))
+
+if a > b and a > c:
+    print("The largest number is:", a)
+elif b > a and b > c:
+    print("The largest number is:", b)
+else:
+    print("The largest number is:", c)
+
