b07a3e5c087177739175cc9a932940e2c480516b
Aadarshdubey637 <dubeyaadarsh637@gmail.com>
2025-03-06 23:59:26 +0530

    Update pratice.py

--- pratice.py
+++ pratice.py
@@ -8,3 +8,8 @@ print("area of the cicle is",area)
 celsius=float(input("enter temprature in celcius"))
 kelvin=celsius+273.15
 print("temprature in kelvin",kelvin)
+
+(2)#miles is equal to km
+miles=float(input("enter miles:"))
+km=miles/0.621371
+print(miles,"is eual to",km,"kilometer")
