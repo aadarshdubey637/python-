cfb8a7f4ee41378afc325353057acafe6522f496
Aadarshdubey637 <dubeyaadarsh637@gmail.com>
2025-03-06 23:53:45 +0530

    Update and rename Area of cicle.py to pratice.py

---
+++ pratice.py
@@ -0,0 +1,10 @@
+(1) #calculate area of circle
+import math
+radius=float(input("enter radius:"))
+area= math.pi*radius**2
+print("area of the cicle is",area)
+
+(2) temprature in kelvin
+celsius=float(input("enter temprature in celcius"))
+kelvin=celsius+273.15
+print("temprature in kelvin",kelvin)
